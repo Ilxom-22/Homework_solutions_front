152c6558b3554db7497344ede5c62ca57e556a78
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-11 01:17:29 +0500

    Add todo card and todo form

---
+++ src/83_lesson/ToDo/src/modules/todos/components/NewTodoForm.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <div>
+        <form class="w-full">
+            <input type="text" placeholder="Add a task" class="input task-background theme-text"/>
+        </form>
+    </div>
+
+</template>
---
+++ src/83_lesson/ToDo/src/modules/todos/components/TodoCard.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <div class="flex w-full p-1 px-3 border rounded-md h-14 task-background gap-x-4 theme-text">
+       
+        <!-- Primary Action -->
+        <div class="flex items-center">
+            <button class="w-5 h-5 border-2 rounded-full theme-border">
+            </button>  
+        </div>
+
+        <!-- Details -->
+        <div class="">
+            <h5 class="theme-text">Task</h5>
+            
+            <div class="flex gap-2 text-sm theme-textSecondary">
+                <p class="opacity-80">Sunday</p>
+                <span class="opacity-50">•</span>
+                <p class="opacity-40">10pm</p>
+            </div>
+        </div>
+
+        <!-- Secondary Actions -->
+        <div>
+            <button class="">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+
+            <button>
+                
+            </button>
+
+            <button>
+                
+            </button>
+        </div>
+        
+       
+    </div>
+
+</template>
