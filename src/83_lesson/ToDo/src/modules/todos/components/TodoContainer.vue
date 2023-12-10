8ee639b23f4e234f7981195a1e8686aa839b132c
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-11 01:17:48 +0500

    Add todo list and todo container

---
+++ src/83_lesson/ToDo/src/modules/todos/components/TodoContainer.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="p-20 px-[10%] flex flex-col gap-y-20 w-full h-full justify-between">
+         <!-- Todo list -->
+         <todo-list/>
+
+        <!-- Add New Task -->
+        <new-todo-form/>
+    </div>
+   
+
+</template>
+
+<script setup lang="ts">
+
+    import TodoList from './TodoList.vue';
+    import NewTodoForm from './NewTodoForm.vue';
+
+</script>
---
+++ src/83_lesson/ToDo/src/modules/todos/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="flex flex-col gap-3 overflow-x-scroll no-scrollbar">
+        
+        <!-- Tasks -->
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        <todo-card/>
+        
+    </div>
+
+</template>
+
+<script setup lang="ts">
+
+    import TodoCard from "./TodoCard.vue"
+
+</script>
