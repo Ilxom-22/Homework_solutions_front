56d55155217989524545f475255a40a5e4d6e6fa
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 00:55:07 +0500

    Enable adding new task

--- src/83_lesson/ToDo/src/modules/todos/components/NewTodoForm.vue
+++ src/83_lesson/ToDo/src/modules/todos/components/NewTodoForm.vue
@@ -1,9 +1,49 @@
 <template>
 
     <div>
-        <form class="w-full">
-            <input type="text" placeholder="Add a task" class="input task-background theme-text"/>
+        <form @submit.prevent="submitAsync" class="w-full">
+            <input type="text" v-model="todo.title" placeholder="Add a task" class="input task-background theme-text"/>
         </form>
     </div>
 
 </template>
+
+<script setup lang="ts">
+
+import { TodoApiClient } from '@/infrastructure/apiClients/todoApiClient/brokers/TodoApiClient';
+import { ToDoItem } from '../models/ToDoItem';
+import { ref } from 'vue';
+
+
+const todoApiClient = new TodoApiClient();
+
+const todo = ref<ToDoItem>(new ToDoItem());
+
+const emit = defineEmits<{
+    addNewTodo: [todo: ToDoItem]
+}>();
+
+const createToDoAsync = async () => {
+    const response = await todoApiClient.todos.createAsync(todo.value);
+
+    if (response.IsSuccess)
+        emit("addNewTodo", response.response!);
+    
+    return response.IsSuccess;
+}
+
+const resetForm = () => {
+    todo.value = new ToDoItem();
+}
+
+const submitAsync = async () => {
+    let result: boolean;
+
+    result = await createToDoAsync();
+
+    if (result) {
+        resetForm();
+    }
+}
+
+</script>
