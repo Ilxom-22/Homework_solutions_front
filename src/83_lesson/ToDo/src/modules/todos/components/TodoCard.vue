f000dda8603374acad2bbc35186a1944d0ea796f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-12 13:34:04 +0500

    Add icons to the todo card

--- src/83_lesson/ToDo/src/modules/todos/components/TodoCard.vue
+++ src/83_lesson/ToDo/src/modules/todos/components/TodoCard.vue
@@ -1,38 +1,43 @@
 <template>
 
-    <div class="flex w-full p-1 px-3 border rounded-md h-14 task-background gap-x-4 theme-text">
+    <div class="flex justify-between w-full p-1 px-3 border rounded-md h-14 task-background theme-text">
        
-        <!-- Primary Action -->
-        <div class="flex items-center">
-            <button class="w-5 h-5 border-2 rounded-full theme-border">
-            </button>  
-        </div>
-
-        <!-- Details -->
-        <div class="">
-            <h5 class="theme-text">Task</h5>
-            
-            <div class="flex gap-2 text-sm theme-textSecondary">
-                <p class="opacity-80">Sunday</p>
-                <span class="opacity-50">•</span>
-                <p class="opacity-40">10pm</p>
+       <div class="flex gap-x-4">
+             <!-- Primary Action -->
+            <div class="flex items-center">
+                <button class="w-5 h-5 border-2 rounded-full theme-border">
+                </button>  
             </div>
-        </div>
+
+            <!-- Details -->
+            <div class="">
+                <h5 class="theme-text">Task</h5>
+                
+                <div class="flex gap-2 text-sm theme-textSecondary">
+                    <p class="opacity-80">Sunday</p>
+                    <span class="opacity-50">•</span>
+                    <p class="opacity-40">10pm</p>
+                </div>
+            </div>
+       </div>
 
         <!-- Secondary Actions -->
-        <div>
-            <button class="">
+        
+        <div class="flex text-lg gap-x-2">
+            <button>
+                <i class="fa-solid fa-star"></i>
+            </button>
+
+            <button>
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+
+            <button>
                 <i class="fa-solid fa-trash"></i>
             </button>
-
-            <button>
-                
-            </button>
-
-            <button>
-                
-            </button>
         </div>
+    
+        
         
        
     </div>
