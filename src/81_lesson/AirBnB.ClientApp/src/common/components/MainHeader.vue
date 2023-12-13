6333a031722d971c2df8822c72717e465df260cd
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 18:45:10 +0500

    Add Location category card component

--- src/81_lesson/AirBnB.ClientApp/src/common/components/MainHeader.vue
+++ src/81_lesson/AirBnB.ClientApp/src/common/components/MainHeader.vue
@@ -1,7 +1,19 @@
 <template>
 
-    <div>
+    <div class="fixed top-0 z-10 flex items-center justify-between w-full h-20 border-b content-padding border-borderSecondary bg-defaultBackground text-textPrimary">
+        <!-- Main Logo -->
         <main-logo/>
+
+        <!-- Locations Menu -->
+        <locations-menu/>
+
+        <!-- Profile Menu -->
+        <profile-menu/>
+        
+        <!-- Locations Filter -->
+        <locations-filter/>
+
+        <!-- Bottom Border -->
     </div>
 
 </template>
@@ -9,5 +21,8 @@
 <script setup lang="ts">
 
 import MainLogo from './icons/MainLogo.vue';
+import LocationsMenu from "@/modules/locations/components/LocationsMenu.vue";
+import ProfileMenu from '@/modules/locations/components/ProfileMenu.vue';
+import LocationsFilter from '@/modules/locations/components/LocationsFilter.vue';
 
 </script>
---
+++ src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationCategoryCard.vue
@@ -0,0 +1,9 @@
+<template>
+
+<div class="h-[80px] min-w-[50px] flex flex-col justify-center items-center group select-common">
+    <img class="h-[24px] w-[24px] mb-2 selected-logo-img select-transition whitespace-nowrap" src="https://a0.muscache.com/pictures/50861fca-582c-4bcc-89d3-857fb7ca6528.jpg" alt="Design icon">
+    <h5 class="text-xs font-medium selected-text select-transition whitespace-nowrap">Design</h5>
+    <div class="mt-[10px] h-[2px] w-4/5 selected-underline select-transition whitespace-nowrap"></div>
+</div>
+
+</template>
