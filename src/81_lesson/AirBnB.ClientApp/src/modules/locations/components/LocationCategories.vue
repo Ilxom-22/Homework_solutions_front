8652ec406d9195006d49adec791541664264159c
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 19:13:42 +0500

    Add location categories component

---
+++ src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationCategories.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div class="flex gap-10 overflow-x-scroll md:gap-12 no-scrollbar">
+    
+    <location-category-card/>
+    <location-category-card/>
+    <location-category-card/>
+    <location-category-card/>
+    <location-category-card/>
+    <location-category-card/>
+    
+</div>
+
+</template>
+
+<script setup lang="ts">
+
+import LocationCategoryCard from './LocationCategoryCard.vue';
+
+</script>
