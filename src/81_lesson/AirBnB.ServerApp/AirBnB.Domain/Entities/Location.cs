b81fa9225c58195ec37649a2bc5576ce115d5f35
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 08:22:47 +0500

    Create location entity

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Domain/Entities/Location.cs
@@ -0,0 +1,18 @@
+﻿using AirBnB.Domain.Common.Entities;
+
+namespace AirBnB.Domain.Entities;
+
+public class Location : Entity
+{
+    public string Name { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public string CategoryId { get; set; }
+
+    public int BuiltYear { get; set; }
+
+    public decimal PricePerNight { get; set; }
+
+    public double Rating { get; set; }
+}
