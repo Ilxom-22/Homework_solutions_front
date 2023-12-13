a19a23e8e78e0263884d2a164018732669063de8
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 14:41:20 +0500

    Add todo item configurations

---
+++ src/83_lesson/Todo.ServerApp/Todo.Persistence/EntityConfigurations/TodoItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Todo.Domain.Entities;
+
+namespace Todo.Persistence.EntityConfigurations;
+
+public class TodoItemConfiguration : IEntityTypeConfiguration<TodoItem>
+{
+    public void Configure(EntityTypeBuilder<TodoItem> builder)
+    {
+        builder.Property(todo => todo.Title).IsRequired().HasMaxLength(128);
+    }
+}
