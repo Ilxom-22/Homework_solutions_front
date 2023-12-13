2a36a60ea59bb65ef755f2f6025110abd17adff4
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 19:13:55 +0500

    Add locations grid component

---
+++ src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationsGrid.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div class="mt-[160px] grid grid-cols-1 place-items-center gap-x-4 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+
+    <location-card/>
+    <location-card/>
+    <location-card/>
+    <location-card/>
+    <location-card/>
+
+</div>
+
+</template>
+
+<script setup lang="ts">
+
+import LocationCard from './LocationCard.vue';
+
+</script>
