177595dd8435f5a7dbb575f39ee5432ad1362814
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 08:07:09 +0500

    Add blank host configurations extensions

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+
+namespace ToDo.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static readonly ICollection<Assembly> Assemblies;
+
+    static HostConfiguration()
+    {
+        Assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+        Assemblies.Add(Assembly.GetExecutingAssembly());
+    }
+
+    private static WebApplicationBuilder AddValidators(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddMappers(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddBusinessLogicInfrastructure(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddCors(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(
+            options => { options.AddDefaultPolicy(policyBuilder => { policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }); }
+        );
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+
+        return builder;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        return app;
+    }
+}
