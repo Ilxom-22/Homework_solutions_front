a9bc502284c4a9e76ae1efa4074c17a6f1de7e51
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 14:07:28 +0500

    Create common entity interfaces and abstract classes

---
+++ src/83_lesson/Todo.ServerApp/Todo.Domain/Common/Entities/AuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo.Domain.Common.Entities;
+
+public abstract class AuditableEntity : Entity, IAuditableEntity
+{
+    public DateTimeOffset CreatedTime { get; set; }
+
+    public DateTimeOffset? ModifiedTime { get; set; }
+}
