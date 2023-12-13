15e6d13d59c1423089c5d4f878ddac3b9c576da6
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 14:08:24 +0500

    Add TodoItem entity

---
+++ src/83_lesson/Todo.ServerApp/Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using Todo.Domain.Common.Entities;
+
+namespace Todo.Domain.Entities;
+
+public class TodoItem : AuditableEntity
+{
+    public string Title { get; set; } = default!;
+
+    public bool IsDone { get; set; }
+
+    public bool IsFavorite { get; set; }
+
+    public DateTimeOffset DueTime { get; set; }
+
+    public DateTimeOffset ReminderTime { get; set; }
+}
