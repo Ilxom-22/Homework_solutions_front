9677fb328d284f4ec2a96bf3dd1f34ffba574e9c
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-13 08:27:33 +0500

    Create category entity

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using AirBnB.Domain.Common.Entities;
+
+namespace AirBnB.Domain.Entities;
+
+public class Category : Entity
+{
+    public string Name { get; set; }
+
+    public string ImageUrl { get; set; }
+}
