6ef89046310c6173126347fa70a7d145fb350497
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-14 14:38:28 +0500

    Create category dto and category mapping profile

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Api/Mappers/CategoryProfile.cs
@@ -0,0 +1,13 @@
+﻿using AirBnB.Api.Models.Dtos;
+using AirBnB.Domain.Entities;
+using AutoMapper;
+
+namespace AirBnB.Api.Mappers;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+}
---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Api/Models/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AirBnB.Api.Models.Dtos;
+
+public class CategoryDto
+{
+    public string Name { get; set; } = default!;
+
+    public string ImageUrl { get; set; } = default!;
+}
