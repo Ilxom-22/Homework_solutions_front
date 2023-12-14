40e419701df212b937727fb9fc9cb5e605d8e165
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-14 14:38:42 +0500

    Add location dto and location mapping profile

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Api/Models/Dtos/LocationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AirBnB.Api.Models.Dtos;
+
+public class LocationDto
+{
+    public string Name { get; set; } = default!;
+
+    public string ImageUrl { get; set; } = default!;
+
+    public int BuiltYear { get; set; }
+
+    public decimal PricePerNight { get; set; }
+
+    public double Rating { get; set; }
+}
