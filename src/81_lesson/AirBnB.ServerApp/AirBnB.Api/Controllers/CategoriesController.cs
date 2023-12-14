1f679c55233da48df0d5e8dbe16b8a2f4b029a69
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-14 14:42:56 +0500

    Add basic categories controller

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using AirBnB.Api.Models.Dtos;
+using AirBnB.Application.Locations;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirBnB.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController(ICategoryService categoryService, IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public ValueTask<IActionResult> GetAllCategories()
+    {
+        return new ValueTask<IActionResult>
+        (
+            Ok(categoryService
+                .Get()
+                .Select(mapper.Map<CategoryDto>))
+        );
+    }
+}
