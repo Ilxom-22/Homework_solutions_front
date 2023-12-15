1bd47c7a5f68ead3d488ba7f5be4d2fba17fcf38
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-15 18:06:31 +0500

    Enable getting category name and image from airbnb api client

--- src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationCategories.vue
+++ src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationCategories.vue
@@ -2,12 +2,7 @@
 
 <div class="flex gap-10 overflow-x-scroll md:gap-12 no-scrollbar">
     
-    <location-category-card/>
-    <location-category-card/>
-    <location-category-card/>
-    <location-category-card/>
-    <location-category-card/>
-    <location-category-card/>
+    <location-category-card v-for="category in categories" :category="category"/>
     
 </div>
 
@@ -15,6 +10,22 @@
 
 <script setup lang="ts">
 
+import { AirBnbApiClient } from '@/infrastructure/apiClients/airBnbApiClients/brokers/AirBnbApiClient';
+import { Category } from '../models/Category';
 import LocationCategoryCard from './LocationCategoryCard.vue';
+import {onBeforeMount, ref} from 'vue';
+
+const apiClient = new AirBnbApiClient();
+
+const categories = ref<Category[]>([]);
+
+onBeforeMount(async () => {
+   const categoriesResponse = await apiClient.categories.getAsync(); 
+   
+   if (categoriesResponse.response != null)
+        categories.value = categoriesResponse.response;
+});
+
+
 
 </script>
--- src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationCategoryCard.vue
+++ src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationCategoryCard.vue
@@ -1,9 +1,23 @@
 <template>
 
 <div class="h-[80px] min-w-[50px] flex flex-col justify-center items-center group select-common">
-    <img class="h-[24px] w-[24px] mb-2 selected-logo-img select-transition whitespace-nowrap" src="https://a0.muscache.com/pictures/50861fca-582c-4bcc-89d3-857fb7ca6528.jpg" alt="Design icon">
-    <h5 class="text-xs font-medium selected-text select-transition whitespace-nowrap">Design</h5>
+    <img class="h-[24px] w-[24px] mb-2 selected-logo-img select-transition whitespace-nowrap" 
+        :src ="category.imageUrl" 
+        alt="design icon">
+    <h5 class="text-xs font-medium selected-text select-transition whitespace-nowrap">{{ category.name }}</h5>
     <div class="mt-[10px] h-[2px] w-4/5 selected-underline select-transition whitespace-nowrap"></div>
 </div>
 
 </template>
+
+<script setup lang="ts">
+import type { Category } from '../models/Category';
+
+
+const props = defineProps({
+    category: {
+        type: Object as () => Category,
+        required: true
+    }
+});
+</script>
