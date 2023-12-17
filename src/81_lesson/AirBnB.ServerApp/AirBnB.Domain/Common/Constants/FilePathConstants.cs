bf0bdcc1fa7fc429fd6021600649efe277649a76
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-17 11:26:34 +0500

    Add file path constants static model

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Domain/Common/Constants/FilePathConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace AirBnB.Domain.Common.Constants;
+
+public static class FilePathConstants
+{
+    public const string FileNameToken = "{{FileName}}";
+
+    public const string LocationCategoryPath = @$"wwwroot\LocationCategories\{FileNameToken}";
+    public const string LocationCategorySeedDataPath = @"SeedData\LocationCategories\locationCategories.json";
+}
