abba32bd85d0cffd3685c43e8e5d60aa6c3bfdd0
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-17 11:27:13 +0500

    Add http client broker to download images

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Persistence/SeedData/Services/HttpClientBroker.cs
@@ -0,0 +1,30 @@
+﻿namespace AirBnB.Persistence.SeedData.Services;
+
+public static class HttpClientBroker
+{
+    public static async ValueTask DownloadAsync(string url, string fileSavingPath)
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(fileSavingPath) ?? throw new ArgumentNullException(nameof(fileSavingPath), "The given file path cannot be null."));
+
+        using var client = new HttpClient();
+        try
+        {
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var fileBytes = await response.Content.ReadAsByteArrayAsync();
+
+                File.WriteAllBytes(fileSavingPath, fileBytes);
+            }
+            else
+            {
+                await Console.Out.WriteLineAsync("The given URL is either invalid or not responding.");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Unexpected error occured while downloading the file.");
+        }
+    }
+}
