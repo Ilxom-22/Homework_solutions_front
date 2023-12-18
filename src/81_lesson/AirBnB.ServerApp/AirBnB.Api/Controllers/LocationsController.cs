6f36b7e58e4b414dc57968059d1006133d32e3ae
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-18 20:10:32 +0500

    Add Locations controller

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Api/Controllers/LocationsController.cs
@@ -0,0 +1,24 @@
+﻿using AirBnB.Api.Models.Dtos;
+using AirBnB.Api.Settings;
+using AirBnB.Application.Locations;
+using AirBnB.Domain.Common.Extensions;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AirBnB.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LocationsController(ILocationService locationService, IMapper mapper, IOptions<ApiSettings> apiSettings) : ControllerBase
+{
+    [HttpGet("{categoryId}:guid")]
+    public ValueTask<IActionResult> GetLocationsByCategoryId([FromRoute]Guid categoryId)
+    {
+        var result = locationService.GetByCategoryId(categoryId, true).Select(mapper.Map<LocationDto>).ToList();
+
+        result.ForEach(location => location.ImageUrl = location.ImageUrl.ToUrl(apiSettings.Value.BaseUrl));
+
+        return new(Ok(result));
+    } 
+}
