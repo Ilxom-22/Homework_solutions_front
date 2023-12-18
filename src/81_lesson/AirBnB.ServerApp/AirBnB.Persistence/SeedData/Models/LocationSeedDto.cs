bc7539232799f2209055bc44ade47aa1e3b10925
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-18 19:45:03 +0500

    Add location seed dto model for converting raw json data into applicable location model

---
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Persistence/SeedData/Models/LocationSeedDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AirBnB.Persistence.SeedData.Models;
+
+public class LocationSeedDto
+{
+    public string Name { get; set; } = default!;
+
+    public string ImageUrl { get; set; } = default!;
+
+    public decimal Price { get; set; }
+
+    public double Rating { get; set; }
+
+    public string Category { get; set; } = default!;
+}
