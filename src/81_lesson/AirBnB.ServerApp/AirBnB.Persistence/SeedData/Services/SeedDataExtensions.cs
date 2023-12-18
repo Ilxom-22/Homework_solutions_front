4e9e0df4fd79fbdde64a4e039882de7bde77b954
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-18 20:04:29 +0500

    Add seed data extension for initializing location seed data

--- src/81_lesson/AirBnB.ServerApp/AirBnB.Persistence/SeedData/Services/SeedDataExtensions.cs
+++ src/81_lesson/AirBnB.ServerApp/AirBnB.Persistence/SeedData/Services/SeedDataExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using AirBnB.Domain.Common.Constants;
 using AirBnB.Domain.Entities;
 using AirBnB.Persistence.DataContexts;
+using AirBnB.Persistence.SeedData.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
@@ -13,10 +14,13 @@ public static class SeedDataExtensions
     {
         var locationDatabase = provider.GetRequiredService<LocationsDataContext>();
 
+        // Initializing Location Categories.
         if (!await locationDatabase.Categories.AnyAsync())
-        {
             await locationDatabase.InitializeCategoriesAsync();
-        }
+
+        // Initializing Locations.
+        if (!await locationDatabase.Locations.AnyAsync())
+            await locationDatabase.InitializeLocationsAsync();
     }
 
     private static async ValueTask InitializeCategoriesAsync(this LocationsDataContext dbContext)
@@ -24,7 +28,7 @@ public static class SeedDataExtensions
         var rawData = File.ReadAllText(FilePathConstants.LocationCategorySeedDataPath);
 
         var categories = JsonSerializer.Deserialize<List<Category>>(rawData)
-            ?? throw new InvalidOperationException("Cannot serialize given object.");
+            ?? throw new InvalidOperationException("Cannot serialize given location categories.");
 
         foreach (var category in categories)
         {
@@ -41,5 +45,52 @@ public static class SeedDataExtensions
         }
 
        await dbContext.SaveChangesAsync();
+    }
+
+    private static async ValueTask InitializeLocationsAsync(this LocationsDataContext dbContext)
+    {
+        var rawData = File.ReadAllText(FilePathConstants.LocationSeedDataPath);
+        var locationDtos = JsonSerializer.Deserialize<List<LocationSeedDto>>(rawData) ?? throw new InvalidOperationException("Cannot serialize give locations.");
+
+        var categoryName = string.Empty;
+        var initialCategoryId = Guid.Empty;
+
+        foreach (var locationDto in locationDtos)
+        {
+            var id = Guid.NewGuid();
+            var filePath = FilePathConstants.LocationPath
+                .Replace(FilePathConstants.FileNameToken, $"{id}.jpg");
+
+            await HttpClientBroker.DownloadAsync(locationDto.ImageUrl, filePath);
+
+            var categoryId = Guid.Empty;
+
+            if (locationDto.Category == categoryName)
+            {
+                categoryId = initialCategoryId;
+            }
+            else
+            {
+                categoryId = (dbContext.Categories.FirstOrDefault(category => category.Name == locationDto.Category)?.Id)
+                ?? throw new ArgumentException("Category not found!");
+
+                categoryName = locationDto.Category;
+                initialCategoryId = categoryId;
+            }
+
+            var location = new Location
+            {
+                Id = id,
+                Name = locationDto.Name,
+                ImageUrl = filePath,
+                PricePerNight = locationDto.Price / 5,
+                Rating = locationDto.Rating,
+                CategoryId = categoryId
+            };
+
+            await dbContext.Locations.AddAsync(location);
+        }
+
+        await dbContext.SaveChangesAsync();
     }
 }
