572ac71a3a459708b89b0e49601533e832d9f346
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-19 18:55:56 +0500

    Enable locations grid to get location items from backend

--- src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationsGrid.vue
+++ src/81_lesson/AirBnB.ClientApp/src/modules/locations/components/LocationsGrid.vue
@@ -2,11 +2,8 @@
 
 <div class="mt-[160px] grid grid-cols-1 place-items-center gap-x-4 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 
-    <location-card/>
-    <location-card/>
-    <location-card/>
-    <location-card/>
-    <location-card/>
+    <!-- Locations -->
+    <location-card v-for="location in locations" :location="location"/>
 
 </div>
 
@@ -15,5 +12,24 @@
 <script setup lang="ts">
 
 import LocationCard from './LocationCard.vue';
+import { onBeforeMount, ref } from 'vue';
+import { AirBnbApiClient } from '@/infrastructure/apiClients/airBnbApiClients/brokers/AirBnbApiClient';
+import { Guid } from 'guid-typescript';
+import type { Location } from '../models/Location';
+
+const airBnbApiClient = new AirBnbApiClient();
+const locations = ref<Location[]>([]);
+
+
+onBeforeMount(async () => {
+    await loadLocationCardsAsync();
+});
+
+const loadLocationCardsAsync = async () => {
+    const locationsResponse = await airBnbApiClient.locations.getLocations();
+    if (locationsResponse.isSuccess && locationsResponse.response) {
+        locations.value = locationsResponse.response;
+    }
+};
 
 </script>
